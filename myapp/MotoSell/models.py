b822038eeee036630e12e86dcd55ebed7396ceea
Hubert Pogorzelski <hubert.pogorzelski2002@gmail.com>
2024-08-08 13:56:40 +0200

    Dodanie ogranicznikow w bazie danych

--- myapp/MotoSell/models.py
+++ myapp/MotoSell/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
 class Vehicle(models.Model):
     MOTORCYCLE = 'motocykl'
@@ -25,10 +26,18 @@ class Vehicle(models.Model):
     category = models.CharField(max_length=20, choices=VEHICLE_CATEGORY_CHOICES)
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    year = models.PositiveIntegerField()
-    mileage = models.PositiveIntegerField()
-    engine_capacity = models.PositiveIntegerField()
-    power = models.PositiveIntegerField()
+    year = models.PositiveIntegerField(
+        validators=[MaxValueValidator(2025)]  # Year should not exceed 2025
+    )
+    mileage = models.PositiveIntegerField(
+        validators=[MaxValueValidator(1500000)]  # Mileage should not exceed 1.5 million
+    )
+    engine_capacity = models.PositiveIntegerField(
+        validators=[MaxValueValidator(20)]  # Engine capacity should not exceed 20
+    )
+    power = models.PositiveIntegerField(
+        validators=[MaxValueValidator(2000)]  # Power should not exceed 2000
+    )
     fuel_type = models.CharField(max_length=10, choices=FUEL_TYPE_CHOICES)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='vehicles_photos/')
